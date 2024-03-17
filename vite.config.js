669104dc911a91c6212251030b9159f7bc02f9b5
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-03-18 03:00:48 +0900

    components

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,15 @@ import {ViteEjsPlugin} from "vite-plugin-ejs";
 
 export default defineConfig({
   plugins: [
-    // Without Data
-    ViteEjsPlugin(),
     
     // With Data
     ViteEjsPlugin({
       domain: "example.com",
       title: "My vue project!"
     }),
-    
-    // // Or With Vite Config
-    // ViteEjsPlugin((viteConfig) => {
-    //   // viteConfig is the current viteResolved config.
-    //   return {
-    //     root: viteConfig.root,
-    //     domain: "example.com",
-    //     title: "My vue project!"
-    //   }
-    // }),
   ],
   server: {
     watch: {
-      // EJSファイルの変更を監視
       usePolling: true,
     },
   },
