5a60f5436ccee303571bdee36708bd8490480563
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-03-19 02:26:45 +0900

    works

--- src/data/works.js
+++ src/data/works.js
@@ -1,27 +1,27 @@
 export const works = {
   graduationWorks: [
     {
-      title: "SiiD Webサイト",
+      headline: "SiiD Webサイト",
       author: "monpy",
       course: "Basic",
       img: "https://dummyimage.com/724x420/000/fff",
     },
     {
-      title: "SiiD Webサイト",
-      author: "monpy",
-      course: "Basic",
-      img: "https://dummyimage.com/724x420/000/fff",
-      desc: "エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。",
-    },
-    {
-      title: "SiiD Webサイト",
+      headline: "SiiD Webサイト",
       author: "monpy",
       course: "Basic",
       img: "https://dummyimage.com/724x420/000/fff",
       desc: "エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。",
     },
     {
-      title: "SiiD Webサイト",
+      headline: "SiiD Webサイト",
+      author: "monpy",
+      course: "Basic",
+      img: "https://dummyimage.com/724x420/000/fff",
+      desc: "エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。",
+    },
+    {
+      headline: "SiiD Webサイト",
       author: "monpy",
       course: "Basic",
       img: "https://dummyimage.com/724x420/000/fff",
