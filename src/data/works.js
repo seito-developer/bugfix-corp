eafad4d9d3dadb02825a4a5a7c7b1e8deab2f7f7
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-04-20 05:10:54 +0900

    media OK

--- src/data/works.js
+++ src/data/works.js
@@ -11,14 +11,14 @@ export const works = {
       headline: "SiiD Webサイト",
       author: "monpy",
       course: "Basic",
-      img: "/images/showcase/work-1.png",
+      img: "/images/showcase/work-2.png",
       desc: "エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。",
     },
     {
       headline: "SiiD Webサイト",
       author: "monpy",
       course: "Basic",
-      img: "/images/showcase/work-1.png",
+      img: "/images/showcase/work-3.png",
       desc: "エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。エビフライの主人公をタップで動かす横スクールアクションゲーム。Apple Store, Google Play Storeで遊べます。",
     },
     {
