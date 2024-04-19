c6c5058efeca167a393c13636617936d7f7b9ccc
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-04-20 01:58:08 +0900

    change directory and routing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
-// import { resolve } from 'path'
+import { resolve } from 'path'
 import {defineConfig} from "vite";
 import {ViteEjsPlugin} from "vite-plugin-ejs";
 import { mainData } from "./src/data/mainData";
 
 export default defineConfig({
-  root: './src',
+  root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    copyPublicDir: 'public',
     rollupOptions: {
+      
       input: {
-        main: './src/index.html',
-        siid: './src/siid/index.html',
+        main: resolve(__dirname, 'index.html'),
+        siid: {
+          "": resolve(__dirname, 'siid/index.html'),
+          basic: resolve(__dirname, 'siid/basic/index.html'),
+          career: resolve(__dirname, 'siid/career/index.html'),
+        }
+        // contact: './src/contact/index.html',
+        // privacyPolicy: './src/privacy-policy/index.html',
+        // law: './src/law/index.html',
+        // siid_basic: './src/siid/basic/index.html',
+        // siid_career: './src/siid/career/index.html',
       }
     }
   },
