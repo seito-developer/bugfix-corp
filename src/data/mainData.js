564c24fe78d4838cb7b744e39f04c2add9cf6054
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-09-20 03:05:51 +0900

    一旦voice paeg

--- src/data/mainData.js
+++ src/data/mainData.js
@@ -1,7 +1,9 @@
 import { common } from "./common";
 import { works } from "./works";
+import { voices } from "./voices";
 
 export const mainData = {
   common: common,
   works: works,
+  voices: voices,
 }
---
+++ src/data/voices.js
@@ -0,0 +1,15 @@
+export const voices = [
+  {
+      name: "R・Nさん",
+      age: "20代",
+      job: "フリーター",
+      result: "未経験からエンジニア職に8社内定！",
+      href: "/voices/ryo.png",
+      img: {
+        url: "/images/voices/ryo.png"
+      },
+      hiddenData: {
+        thoughts: "「0→1」「1→10」「10→100」全て自分で創り上げる楽しさをしれました！<br>IT業界に興味あるけど、何から始めたらいいか分からない、自信がないって方におすすめです！！"
+      }
+    }
+];
