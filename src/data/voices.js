9db3c7dd9dd62173cbf9ea3aa67f0b651d060810
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-09-20 10:47:47 +0900

    voiceスタイル修正

--- src/data/voices.js
+++ src/data/voices.js
@@ -1,13 +1,58 @@
 export const voices = [
   {
-      name: "R・Nさん",
-      age: "20代",
-      job: "フリーター",
-      result: "未経験からエンジニア職に8社内定！",
-      // href: "/voices/ryo.png",
-      img: "/images/voices/ryo.png",
-      hiddenData: {
-        thoughts: "「0→1」「1→10」「10→100」全て自分で創り上げる楽しさをしれました！<br>IT業界に興味あるけど、何から始めたらいいか分からない、自信がないって方におすすめです！！"
-      }
+    name: "R・Nさん",
+    age: "20代",
+    job: "フリーター",
+    course: "Career",
+    result: "未経験からエンジニア職に8社内定！",
+    img: "/images/voices/ryo.png",
+    hiddenData: {
+      thoughts: "「0→1」「1→10」「10→100」全て自分で創り上げる楽しさをしれました！<br>IT業界に興味あるけど、何から始めたらいいか分からない、自信がないって方におすすめです！！",
+      feedback: ""
     }
+  }, {
+    name: "R・Nさん",
+    age: "20代",
+    job: "フリーター",
+    course: "Career",
+    result: "未経験からエンジニア職に8社内定！",
+    img: "/images/voices/ryo.png",
+    hiddenData: {
+      thoughts: "「0→1」「1→10」「10→100」全て自分で創り上げる楽しさをしれました！<br>IT業界に興味あるけど、何から始めたらいいか分からない、自信がないって方におすすめです！！",
+      feedback: ""
+    }
+  }, {
+    name: "R・Nさん",
+    age: "20代",
+    job: "フリーター",
+    course: "Career",
+    result: "未経験からエンジニア職に8社内定！",
+    img: "/images/voices/ryo.png",
+    hiddenData: {
+      thoughts: "「0→1」「1→10」「10→100」全て自分で創り上げる楽しさをしれました！<br>IT業界に興味あるけど、何から始めたらいいか分からない、自信がないって方におすすめです！！",
+      feedback: ""
+    }
+  }, {
+    name: "R・Nさん",
+    age: "20代",
+    job: "フリーター",
+    course: "Career",
+    result: "未経験からエンジニア職に8社内定！",
+    img: "/images/voices/ryo.png",
+    hiddenData: {
+      thoughts: "「0→1」「1→10」「10→100」全て自分で創り上げる楽しさをしれました！<br>IT業界に興味あるけど、何から始めたらいいか分からない、自信がないって方におすすめです！！",
+      feedback: ""
+    }
+  }, {
+    name: "けんけんさん",
+    age: "20代",
+    job: "倉庫業",
+    course: "Career",
+    result: "地方で未経験からエンジニア職に内定！",
+    img: "/images/voices/man1.png",
+    hiddenData: {
+      thoughts: "苦手な転職活動をがっつりサポートしていただくために入校しました。特に自己分析、書類作成、面接対策の手厚さとクオリティに感動しました！転職を少しでも良い方向に進めたい方にオススメです！",
+      feedback: "けんけんさんはとくに学習熱心で、最終的に高品質なポートフォリオを3つも作成されたのが印象的でした。現在はその経験を活かして、副業でSiiDのアシスタント講師をしていただいています◎"
+    }
+  }, 
 ];
