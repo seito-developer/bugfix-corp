119df4ced82e23869cb5a7fc04b130d855bc0343
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-11-05 10:32:41 +0900

    updated: gifts

---
+++ src/pages/assets/scripts/gifts-1.js
@@ -0,0 +1,4 @@
+const currentUrl = window.location.href;
+if (!currentUrl.includes('key=fromline20241105')) {
+    window.location.href = '/';
+}
--- src/pages/assets/scripts/gifts.js
+++
@@ -1,4 +0,0 @@
-const currentUrl = window.location.href;
-if (!currentUrl.includes('key=fromline20240901')) {
-    window.location.href = '/';
-}
