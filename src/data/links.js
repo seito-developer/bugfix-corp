a4bb92c318604338a54ce78c585d5be83f5beb96
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-06-08 18:11:53 +0900

    fix lp

---
+++ src/data/links.js
@@ -0,0 +1,3 @@
+export const links = {
+    line: "https://liff.line.me/2006287461-mwL4LrXp/follow/01JW54BCQV8W1QEZ393Z9G916W/",
+}
--- src/data/mainData.js
+++ src/data/mainData.js
@@ -1,9 +1,11 @@
 import { common } from "./common";
 import { works } from "./works";
 import { voices } from "./voices";
+import { links } from "./links";
 
 export const mainData = {
   common: common,
   works: works,
   voices: voices,
+  links: links
 }
