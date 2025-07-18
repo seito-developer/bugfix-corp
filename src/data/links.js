ddfe2b211d75ec70d03ccb44859067ffb1b69592
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-07-19 00:41:55 +0900

    feat: LINE A8

--- src/data/links.js
+++ src/data/links.js
@@ -1,3 +1,3 @@
 export const links = {
-    line: "https://liff.line.me/2006287461-mwL4LrXp/follow/01JW54BCQV8W1QEZ393Z9G916W/",
+    line: "https://liff.line.me/2007708056-2vY014Yy/follow/01K03JQ0BN0T3TCXJRAV87GCEN/",
 }
