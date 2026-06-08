55940610f14f29fb41aeece4db7c31dc0181be7c
Seito Horiguchi <seito.horiguchi@gmail.com>
2026-06-09 01:39:31 +0900

    js修正

--- src/pages/siid/lp-1/assets/script.js
+++ src/pages/siid/lp-1/assets/script.js
@@ -9,14 +9,19 @@ const first = document.querySelector(".faq-item");
 if (first) first.classList.add("open");
 
 // js-float-cta
-// 下にスクロールしたら表示、上にスクロールしたら非表示
-const cta = document.querySelector("#js-float-cta");
-let lastScrollY = window.scrollY;
-window.addEventListener("scroll", () => {
-  if (window.scrollY > lastScrollY) {
-    cta.classList.add("is-active");
-  } else {
-    cta.classList.remove("is-active");
+// スマホのみ有効。下にスクロールしたら表示、上にスクロールしたら非表示
+if (window.innerWidth <= 750) {
+  const cta = document.querySelector("#js-float-cta");
+  let lastScrollY = window.scrollY;
+
+  if (cta) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > lastScrollY) {
+        cta.classList.add("is-active");
+      } else {
+        cta.classList.remove("is-active");
+      }
+      lastScrollY = window.scrollY;
+    });
   }
-  lastScrollY = window.scrollY;
-});
+}
